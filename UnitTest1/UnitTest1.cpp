887472f68ea672681135bdbee6f9adad483ea439
Nagumanova01 <64163169+Nagumanova01@users.noreply.github.com>
2020-12-16 17:00:43 +0300

    Add files via upload

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../6 taaask/6 taaask/6 taaask.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			auto ans = F({ { 2, 3 }, {1,2} });
+			vector<pair<int, int>> realAns = { {1, 2}, {2, 3} };
+			Assert::AreEqual(realAns.size(), ans.size());
+			for (int i = 0; i < realAns.size(); ++i) 
+			{
+				Assert::AreEqual(realAns[i].first, ans[i].first);
+				Assert::AreEqual(realAns[i].second, ans[i].second);
+			}
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			auto ans = F({ { 1, 1 }, {1, 2}, {3, 5} });
+			vector<pair<int, int>> realAns = { {1, 1}, {1, 2}, {3,5} };
+			Assert::AreEqual(realAns.size(), ans.size());
+			for (int i = 0; i < realAns.size(); ++i)
+			{
+				Assert::AreEqual(realAns[i].first, ans[i].first);
+				Assert::AreEqual(realAns[i].second, ans[i].second);
+			}
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			auto ans = F({ { 8, 7 }, {1, 3}, {3, 5} });
+			vector<pair<int, int>> realAns = { {1, 3}, {3, 5}, {8, 7} };
+			Assert::AreEqual(realAns.size(), ans.size());
+			for (int i = 0; i < realAns.size(); ++i)
+			{
+				Assert::AreEqual(realAns[i].first, ans[i].first);
+				Assert::AreEqual(realAns[i].second, ans[i].second);
+			}
+		}
+	};
+}
